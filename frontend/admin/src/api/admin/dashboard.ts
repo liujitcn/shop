b786dd0554302207aee4fcfb23b6ca1ed6a9fc08
210583 <liujun@newcapec.cn>
2026-03-28 22:17:54 +0800

    优化变量定义

--- frontend/admin/src/api/admin/dashboard.ts
+++ frontend/admin/src/api/admin/dashboard.ts
@@ -12,14 +12,14 @@ import {
   type DashboardService,
 } from "@/rpc/admin/dashboard";
 
-const GOODS_URL = "/admin/dashboard";
+const ADMIN_DASHBOARD = "/admin/dashboard";
 
 /** Admin首页服务 */
 export class DashboardServiceImpl implements DashboardService {
   /** 查询汇总数据（用户） */
   DashboardCountUser(request: DashboardCountRequest): Promise<DashboardCountResponse> {
     return service<DashboardCountRequest, DashboardCountResponse>({
-      url: `${GOODS_URL}/count/user`,
+      url: `${ADMIN_DASHBOARD}/count/user`,
       method: "get",
       params: request,
     });
@@ -27,7 +27,7 @@ export class DashboardServiceImpl implements DashboardService {
   /** 查询汇总数据（商品） */
   DashboardCountGoods(request: DashboardCountRequest): Promise<DashboardCountResponse> {
     return service<DashboardCountRequest, DashboardCountResponse>({
-      url: `${GOODS_URL}/count/goods`,
+      url: `${ADMIN_DASHBOARD}/count/goods`,
       method: "get",
       params: request,
     });
@@ -35,7 +35,7 @@ export class DashboardServiceImpl implements DashboardService {
   /** 查询汇总数据（订单） */
   DashboardCountOrder(request: DashboardCountRequest): Promise<DashboardCountResponse> {
     return service<DashboardCountRequest, DashboardCountResponse>({
-      url: `${GOODS_URL}/count/order`,
+      url: `${ADMIN_DASHBOARD}/count/order`,
       method: "get",
       params: request,
     });
@@ -43,7 +43,7 @@ export class DashboardServiceImpl implements DashboardService {
   /** 查询汇总数据（销量） */
   DashboardCountSale(request: DashboardCountRequest): Promise<DashboardCountResponse> {
     return service<DashboardCountRequest, DashboardCountResponse>({
-      url: `${GOODS_URL}/count/sale`,
+      url: `${ADMIN_DASHBOARD}/count/sale`,
       method: "get",
       params: request,
     });
@@ -51,7 +51,7 @@ export class DashboardServiceImpl implements DashboardService {
   /** 查询订单销量（柱状图） */
   DashboardBarOrder(request: DashboardBarOrderRequest): Promise<DashboardBarResponse> {
     return service<DashboardBarOrderRequest, DashboardBarResponse>({
-      url: `${GOODS_URL}/bar/order`,
+      url: `${ADMIN_DASHBOARD}/bar/order`,
       method: "get",
       params: request,
     });
@@ -59,7 +59,7 @@ export class DashboardServiceImpl implements DashboardService {
   /** 查询商品销量（柱状图） */
   DashboardBarGoods(request: DashboardBarGoodsRequest): Promise<DashboardBarResponse> {
     return service<DashboardBarGoodsRequest, DashboardBarResponse>({
-      url: `${GOODS_URL}/bar/goods`,
+      url: `${ADMIN_DASHBOARD}/bar/goods`,
       method: "get",
       params: request,
     });
@@ -67,7 +67,7 @@ export class DashboardServiceImpl implements DashboardService {
   /** 查询商品分类（饼状图） */
   DashboardPieGoods(request: DashboardPieGoodsRequest): Promise<DashboardPieResponse> {
     return service<DashboardPieGoodsRequest, DashboardPieResponse>({
-      url: `${GOODS_URL}/pie/goods`,
+      url: `${ADMIN_DASHBOARD}/pie/goods`,
       method: "get",
       params: request,
     });
@@ -75,7 +75,7 @@ export class DashboardServiceImpl implements DashboardService {
   /** 查询商品订单销量状态（雷达图） */
   DashboardRadarOrder(request: DashboardRadarOrderRequest): Promise<DashboardRadarResponse> {
     return service<DashboardRadarOrderRequest, DashboardRadarResponse>({
-      url: `${GOODS_URL}/radar/order`,
+      url: `${ADMIN_DASHBOARD}/radar/order`,
       method: "get",
       params: request,
     });
